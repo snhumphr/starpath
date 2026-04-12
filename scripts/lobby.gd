daafdf04110a01e1af65819c80bbbc0421f43582
snhumphr <humphreynsimon@gmail.com>
2026-04-12 16:12:08 -0600

    More work on main menu/laying the foundation for multiplayer

---
+++ scripts/lobby.gd
@@ -0,0 +1,5 @@
+extends Control
+
+func init() -> void:
+	
+	pass
--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,15 +1,34 @@
 extends Control
 
 var current_profile: Dictionary
+@onready var JoinRow: PanelContainer = self.get_node("MenuBar/VBoxContainer/JoinRow")
 @onready var ProfileBox: PanelContainer = self.get_node("MenuBar/ProfileBox")
 
+const PORT: int = 7653
 const PROFILE_PATH = "user://profile.json"
+const JOIN_IP_PATH = "user://joinip.txt"
+var host_IP: String
+var join_IP: String
 
 func _ready() -> void:
 	
 	#load profile from disk, if one exists
 	await get_tree().process_frame
 	var current_profile = self.load_profile_from_disk()
+	
+	if OS.has_feature("windows"):
+		host_IP = IP.resolve_hostname(str(OS.get_environment("COMPUTERNAME")), IP.TYPE_IPV4)
+	else: #This needs testing on non-windows devices
+		host_IP = IP.get_local_addresses()[0] #TODO: likely needs serious filtering
+		
+	print("HOST IP: " + self.host_IP)
+		
+	if FileAccess.file_exists(JOIN_IP_PATH):
+		var file = FileAccess.open(JOIN_IP_PATH, FileAccess.READ)
+		var file_text: String = file.get_as_text()
+		file.close()
+		file_text = file_text.strip_edges()
+		self.JoinRow.get_node("MarginContainer/HBoxContainer/LineEdit").set_text(file_text)
 
 func get_profile_from_box() -> Dictionary:
 	
@@ -37,14 +56,41 @@ func load_profile_from_disk() -> Dictionary:
 	
 	return profile
 
+func swap_to_lobby() -> void:
+	
+	self.get_node("MenuBar").set_visible(false)
+	
+	var lobby_scene = load("res://scenes/queued_action.tscn")
+	var instance = lobby_scene.instantiate()
+	instance.init()
+	self.add_child(instance)
+
 func _on_save_button_pressed() -> void:
 	
 	#load the profile from the profile box to current_profile
 	self.current_profile = self.get_profile_from_box()
 
 	#then try to save profile to disk
-	print(PROFILE_PATH)
 	var file = FileAccess.open(PROFILE_PATH, FileAccess.WRITE)
 	var json_string: String = JSON.stringify(self.current_profile)
 	file.store_string(json_string)
 	file.close()
+
+func _on_join_button_pressed() -> void:
+	
+	self.join_IP = self.JoinRow.get_node("MarginContainer/HBoxContainer/LineEdit").get_text()
+	self.join_IP = self.join_IP.strip_edges()
+	
+	if self.join_IP != "":
+		var file = FileAccess.open(JOIN_IP_PATH, FileAccess.WRITE)
+		file.store_string(self.join_IP)
+		
+		var peer = ENetMultiplayerPeer.new()
+		peer.create_client(self.join_IP, PORT)
+		multiplayer.multiplayer_peer = peer
+
+func _on_host_button_pressed() -> void:
+	var peer: ENetMultiplayerPeer = ENetMultiplayerPeer.new()
+	var max_connections: int = 6 #TODO: placeholder value; not sure if 6 players works properly right now
+	peer.create_server(PORT, max_connections)
+	multiplayer.multiplayer_peer = peer
