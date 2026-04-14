ea417f8f1232353ca9e6b7d6475b1e9d5ce05028
snhumphr <humphreynsimon@gmail.com>
2026-04-14 15:39:49 -0600

    More work on the lobby

--- scripts/lobby.gd
+++ scripts/lobby.gd
@@ -1,6 +1,7 @@
 extends Control
 
-var players: Array[Player]
+var players: Dictionary
+var factions: Array[Faction]
 
 func init(player_name: String, player_colour: Color) -> void:
 
@@ -9,42 +10,59 @@ func init(player_name: String, player_colour: Color) -> void:
 	print(multiplayer.is_server())
 	print(multiplayer.get_unique_id())
 	
+	self.factions = self.init_factions()
+	
 	rpc("add_player", player_name, player_colour.r, player_colour.g, player_colour.b)
+
+func init_factions() -> Array[Faction]:
+	
+	var new_factions: Array[Faction] = []
+	
+	var eater_faction: Faction = Eaters.new()
+	eater_faction.init(0) #TODO: check that this doesn't break anything!
+	new_factions.append(eater_faction)
+	
+	return new_factions
 
 func update_players_column():
 	
-	for player in players:
-		if player.network_id == multiplayer.get_unique_id():
-			pass
+	for key in players.keys():
+		var player: Player = players[key]
+		if key == multiplayer.get_unique_id():
+			self.add_player_card(player, true)
 		else:
-			pass
+			self.add_player_card(player, false)
+
+func add_player_card(player: Player, editing_allowed: bool = false) -> void:
+	var card_scene = load("res://scenes/player_card.tscn")
+	var instance = card_scene.instantiate()
+	instance.init(player, self.factions, editing_allowed)
+	self.get_node("MarginContainer/HBoxContainer/Lobby/Control/PlayersCol").add_child(instance)
 
 @rpc("any_peer", "call_local", "reliable")
 func add_player(player_name: String, r: float, g: float, b: float) -> void:
 	var new_player: Player = Player.new()
 	#var new_player: Dictionary = {}
 	new_player.network_id = multiplayer.get_remote_sender_id()
-	new_player.player_id = self.players.size() +1 #TODO: MAKE ABSOLUTELY SURE THAT ALL PLAYER IDS ARE CONSISTENT!!!!
 	new_player.player_name = player_name
-	new_player.faction_id = Faction.FACTION_IDS.NONE
+	new_player.faction_id = self.factions[0].fac_id
 	new_player.colour = Color(r, g, b)
-	self.players.append(new_player)
+	if not players.has(multiplayer.get_remote_sender_id()):
+		self.players[multiplayer.get_remote_sender_id()] = new_player
+	else:
+		printerr("Tried to add duplicate player")
 	self.update_players_column()
 	#print(new_player)
 
 @rpc("any_peer", "call_local", "reliable")
-func set_player(player_id: int, new_faction_id: Faction.FACTION_IDS = Faction.FACTION_IDS.NONE, new_name: String = "", r: float = 99.0, g: float = 99.0, b: float = 99.0) -> void:
+func set_player(new_faction_id: Faction.FACTION_IDS = Faction.FACTION_IDS.NONE, new_name: String = "", r: float = 99.0, g: float = 99.0, b: float = 99.0) -> void:
 	
-	var player: Player = self.players[player_id-1]
-	var sender_id: int = multiplayer.get_remote_sender_id()
+	var player: Player = self.players[multiplayer.get_remote_sender_id()]
 	
-	if sender_id == player.network_id:
-		if new_faction_id != Faction.FACTION_IDS.NONE:
-			player.faction_id = new_faction_id
-		if new_name != "": #TODO: check that the name isn't already in use before changing this
-			player.new_name = new_name 
-		if r < 1.0 and g < 1.0 and b < 1.0:
-			player.colour = Color(r, g, b)
-		self.update_players_column()
-	else:
-		printerr(" wrong network id used to modify player")
+	if new_faction_id != Faction.FACTION_IDS.NONE:
+		player.faction_id = new_faction_id
+	if new_name != "": #TODO: check that the name isn't already in use before changing this
+		player.new_name = new_name 
+	if r < 1.0 and g < 1.0 and b < 1.0:
+		player.colour = Color(r, g, b)
+	self.update_players_column()
--- scripts/player_card.gd
+++ scripts/player_card.gd
@@ -1,4 +1,24 @@
 extends MarginContainer
 
-func init() -> void:
-	pass
+func init(player: Player, factions: Array[Faction], editing_allowed: bool) -> void:
+	
+	var name_row: Node = self.get_node("PanelContainer/VBoxContainer/NameRow")
+	var colour_row: Node = self.get_node("PanelContainer/VBoxContainer/ColourRow")
+	var faction_row: Node = self.get_node("PanelContainer/VBoxContainer/FactionRow")
+	
+	colour_row.get_node("ColourButton").color = player.colour
+	name_row.get_node("NameEdit").set_text(player.player_name)
+	
+	var faction_button: OptionButton = faction_row.get_node("FactionButton")
+	
+	for i in range(factions.size()):
+		var faction: Faction = factions[i]
+		faction_button.add_item(Faction.FACTION_NAMES[faction.fac_id][1], faction.fac_id)
+	
+	faction_button.select(faction_button.get_item_index(player.faction_id))
+	
+	if not editing_allowed:
+		colour_row.get_node("ColourButton").set_disabled(true)
+		name_row.get_node("NameEdit").set_editable(false)
+		faction_button.set_disabled(true)
+		
